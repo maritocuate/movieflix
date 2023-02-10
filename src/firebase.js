47926a74737f7616dd3e409367c3040e39d9c035
marioq <mario300@gmail.com>
2023-02-10 02:35:29 -0300

    chore: setup firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,8 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyDh-PYBa4g5R9TvdkFy-fk8YD1w4s3OE2k",
   authDomain: "movieflix-b947b.firebaseapp.com",
@@ -6,3 +11,10 @@ const firebaseConfig = {
   messagingSenderId: "342868440305",
   appId: "1:342868440305:web:4c6bc72ec06f88c02d2550"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const db = getFirestore()
+
+export { auth, db }
--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,25 +1,50 @@
+import React, { useRef } from 'react'
+import { auth } from '../../firebase'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
 import './styles.scss'
 
 function Signin() {
+    const emailRef = useRef(null)
+    const passwordRef = useRef(null)
 
     const register = (e) => {
         e.preventDefault()
+        
+        createUserWithEmailAndPassword(
+            auth,
+            emailRef.current.value,
+            passwordRef.current.value
+        ).then((authUser) => {
+            console.log(authUser)
+        }).catch(error => {
+            alert(error.message)
+        })
     }
     const signIn = (e) => {
         e.preventDefault()
+
+        signInWithEmailAndPassword(
+            auth,
+            emailRef.current.value,
+            passwordRef.current.value
+        ).then((authUser) => {
+            console.log(authUser)
+        }).catch(error => {
+            alert(error.message)
+        })
     }
 
     return (
         <div className='signin'>
             <h1>Sign In</h1>
             
-            <form onSubmit={signIn}>
-                <input type='email' placeholder='Email' />
-                <input type='password' placeholder='Password' />
+            <form onSubmit={register}>
+                <input type='email' placeholder='Email' ref={emailRef} />
+                <input type='password' placeholder='Password' ref={passwordRef} />
                 <button type='submit'>Sign In</button>
                 <p className='signin__footer'>
                     <span className='signin__gray'>New to Movieflix? </span>
-                    <span className='signin__link' onClick={register}>Sign up now</span>.
+                    <span className='signin__link' onClick={signIn}>Sign up now</span>.
                 </p>
             </form>
         </div>
